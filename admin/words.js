9fb745a18dc952002c2ca3374098f147378b9fac
Scrounger <scrounger@gmx.net>
2019-08-24 16:49:46 +0200

    Custom Dialog: check if entity name is already in use

--- admin/words.js
+++ admin/words.js
@@ -14,18 +14,18 @@ systemDictionary = {
         'pl': 'Ustawienia adaptera dla lovelace',
         'zh-cn': 'lovelace的适配器设置'
     },
-    "Run as:":          {"de": "Laufen unter Anwender:", "ru": "Запустить от пользователя:"},
-    "Allow only when User is Owner:":          {"de": "Nue erlauben wenn Anwender auch Besitzer ist:", "ru": "Allow only when User is Owner:"},
-    "IP:":              {"de": "IP:",                        "ru": "IP:"},
-    "Port:":            {"de": "Port:",                      "ru": "Порт:"},
-    "Secure(HTTPS):":   {"de": "Verschlüsselung(HTTPS):",    "ru": "Шифрование(HTTPS):"},
-    "Authentication:":  {"de": "Authentifikation:",          "ru": "Аутентификация:"},
-    "Public certificate:":    {"en": "Public certificate:",     "de": "Publikzertifikat:",       "ru": "'Public' сертификат:"},
-    "Private certificate:":   {"en": "Private certificate:",    "de": "Privatzertifikat:",       "ru": "'Private' сертификат:"},
-    "Chained certificate:":   {"en": "Chained certificate:",    "de": "Kettenzertifikat:",       "ru": "'Chained' сертификат:"},
-    "Extend WEB adapter:":    {"en": "Extend WEB adapter:",     "de": "Erweitere WEB Adapter:",  "ru": "Подключить к WEB драйверу:"},
-    "all":                    {"en": "all",                     "de": "alle",                    "ru": "все"},
-    "Listen on all IPs":      {"en": "Listen on all IPs",       "de": "Alle IPs zulassen",       "ru": "Открыть для всех IP адресов"},
+    "Run as:": { "de": "Laufen unter Anwender:", "ru": "Запустить от пользователя:" },
+    "Allow only when User is Owner:": { "de": "Nue erlauben wenn Anwender auch Besitzer ist:", "ru": "Allow only when User is Owner:" },
+    "IP:": { "de": "IP:", "ru": "IP:" },
+    "Port:": { "de": "Port:", "ru": "Порт:" },
+    "Secure(HTTPS):": { "de": "Verschlüsselung(HTTPS):", "ru": "Шифрование(HTTPS):" },
+    "Authentication:": { "de": "Authentifikation:", "ru": "Аутентификация:" },
+    "Public certificate:": { "en": "Public certificate:", "de": "Publikzertifikat:", "ru": "'Public' сертификат:" },
+    "Private certificate:": { "en": "Private certificate:", "de": "Privatzertifikat:", "ru": "'Private' сертификат:" },
+    "Chained certificate:": { "en": "Chained certificate:", "de": "Kettenzertifikat:", "ru": "'Chained' сертификат:" },
+    "Extend WEB adapter:": { "en": "Extend WEB adapter:", "de": "Erweitere WEB Adapter:", "ru": "Подключить к WEB драйверу:" },
+    "all": { "en": "all", "de": "alle", "ru": "все" },
+    "Listen on all IPs": { "en": "Listen on all IPs", "de": "Alle IPs zulassen", "ru": "Открыть для всех IP адресов" },
     "Let's Encrypt settings": {
         "en": "Let's Encrypt settings",
         "de": "Einstellungen Let's Encrypt",
@@ -46,10 +46,21 @@ systemDictionary = {
         "de": "Port um die Domain zu prüfen:",
         "ru": "Порт для проверки доменного имени:"
     },
-    "Set certificates or load it first in the system settings (right top).":    {
+    "Set certificates or load it first in the system settings (right top).": {
         "en": "Set certificates or load it first in the system settings (right top).",
         "de": "Setze Zertificate oder lade die erst unter System/Einstellungen (oben rechts).",
         "ru": "Нужно выбрать сертификаты или сначала загрузить их в системных настройках (вверху справа)."
+    },
+    "Entity name '%s' is already in use!": {
+        "en": "Entity name '%s' is already in use!",
+        "de": "Der Entitätsname '%s' wird bereits verwendet!",
+        "ru": "Имя объекта '%s' уже используется!",
+        "pt": "O nome da entidade '%s' já está em uso!",
+        "nl": "Entiteitsnaam '%s' is al in gebruik!",
+        "fr": "Le nom de l'entité '%s' est déjà utilisé!",
+        "it": "Il nome dell'entità '%s' è già in uso!",
+        "es": "El nombre de la entidad '%s' ya está en uso!",
+        "pl": "Nazwa jednostki '%s' jest już w użyciu!",
+        "zh-cn": "实体名称'%s'已在使用中!"
     }
-
 };
