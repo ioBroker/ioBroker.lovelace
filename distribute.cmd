e9d8be110d3b8506489b28f364a77c95c36ae460
Achim Königs <garfonso@mobo.info>
2020-02-01 09:55:51 +0100

    remove accidentially added files

--- distribute.cmd
+++
@@ -1,2 +0,0 @@
-pscp .\lib\server.js achim@server:/tmp/
-putty -ssh achim@server -i D:\PersistenteDaten\Dokumente\KM.ppk -m distribute.sh
--- distribute.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo -Hu iobroker cp /tmp/server.js /opt/iobroker/node_modules/iobroker.lovelace/lib/
-iobroker restart lovelace.1
-
-pause;
