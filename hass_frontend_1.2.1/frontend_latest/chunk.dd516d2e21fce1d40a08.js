7214825bf31aa8123b62540b9fdc739200624283
Achim Königs <garfonso@mobo.info>
2020-06-30 00:30:49 +0200

    remove addition frontend versions
    (sorry)

--- hass_frontend_1.2.1/frontend_latest/chunk.dd516d2e21fce1d40a08.js
+++
@@ -1,2 +0,0 @@
-(self.webpackJsonp=self.webpackJsonp||[]).push([[225],{887:function(e,r,n){"use strict";n.r(r);var t=n(447);n.d(r,"createBadgeElement",(function(){return t.a}));var a=n(314);n.d(r,"createCardElement",(function(){return a.a}));var c=n(377);n.d(r,"createHeaderFooterElement",(function(){return c.a}));var u=n(517);n.d(r,"createHuiElement",(function(){return u.a}));var o=n(384);n.d(r,"createRowElement",(function(){return o.a}))}}]);
-//# sourceMappingURL=chunk.dd516d2e21fce1d40a08.js.map
