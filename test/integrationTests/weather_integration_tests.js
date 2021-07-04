11cd9c6c135d7fbf2d28cad9383c8fb750f21a7e
Garfonso <garfonso@mobo.info>
2021-07-04 13:06:51 +0200

    fix weather tests

--- test/integrationTests/weather_integration_tests.js
+++ test/integrationTests/weather_integration_tests.js
@@ -53,121 +53,151 @@ exports.runTests = function (getHarness) {
             updatedValue: 180
         }, {
             attribute: 'forecast.0.condition',
-            iobId: deviceId + '.WeatherIconURL_d2',
-            initialValue: 'iconURL1',
-            updatedValue: 'iconURL1Updated'
+            iobId: deviceId + '.WeatherIconURL',
+            initialValue: 'iconURL',
+            updatedValue: 'iconURLUpdated'
         }, {
             attribute: 'forecast.0.temperature',
-            iobId: deviceId + '.TempMax_d2',
+            iobId: deviceId + '.TempMax_d1',
             initialValue: 10,
             updatedValue: 11
         }, {
             attribute: 'forecast.0.templow',
-            iobId: deviceId + '.TempMin_d2',
+            iobId: deviceId + '.TempMin_d1',
             initialValue: 8,
             updatedValue: 9
         }, {
             attribute: 'forecast.0.precipitation_probability',
-            iobId: deviceId + '.PrecipitationProbability_d2',
+            iobId: deviceId + '.PrecipitationProbability_d1',
             initialValue: 50,
             updatedValue: 60
         }, {
             attribute: 'forecast.0.precipitation',
-            iobId: deviceId + '.TotalLiquidVolume_d2',
+            iobId: deviceId + '.TotalLiquidVolume_d1',
             initialValue: 500,
             updatedValue: 600
         }, {
             attribute: 'forecast.0.datetime',
-            iobId: deviceId + '.DateTime_d2',
+            iobId: deviceId + '.DateTime_d1',
             initialValue: 'Do',
             updatedValue: 'Fr'
         }, {
             attribute: 'forecast.1.condition',
+            iobId: deviceId + '.WeatherIconURL_d2',
+            initialValue: 'iconURL1',
+            updatedValue: 'iconURL1Updated'
+        }, {
+            attribute: 'forecast.1.temperature',
+            iobId: deviceId + '.TempMax_d2',
+            initialValue: 10,
+            updatedValue: 11
+        }, {
+            attribute: 'forecast.1.templow',
+            iobId: deviceId + '.TempMin_d2',
+            initialValue: 8,
+            updatedValue: 9
+        }, {
+            attribute: 'forecast.1.precipitation_probability',
+            iobId: deviceId + '.PrecipitationProbability_d2',
+            initialValue: 50,
+            updatedValue: 60
+        }, {
+            attribute: 'forecast.1.precipitation',
+            iobId: deviceId + '.TotalLiquidVolume_d2',
+            initialValue: 500,
+            updatedValue: 600
+        }, {
+            attribute: 'forecast.1.datetime',
+            iobId: deviceId + '.DateTime_d2',
+            initialValue: 'Do',
+            updatedValue: 'Fr'
+        }, {
+            attribute: 'forecast.2.condition',
             iobId: deviceId + '.WeatherIconURL_d3',
             initialValue: 'iconURL2',
             updatedValue: 'iconURL2Updated'
         }, {
-            attribute: 'forecast.1.temperature',
+            attribute: 'forecast.2.temperature',
             iobId: deviceId + '.TempMax_d3',
             initialValue: 12,
             updatedValue: 13
         }, {
-            attribute: 'forecast.1.templow',
+            attribute: 'forecast.2.templow',
             iobId: deviceId + '.TempMin_d3',
             initialValue: 7,
             updatedValue: 8
         }, {
-            attribute: 'forecast.1.precipitation_probability',
+            attribute: 'forecast.2.precipitation_probability',
             iobId: deviceId + '.PrecipitationProbability_d3',
             initialValue: 51,
             updatedValue: 61
         }, {
-            attribute: 'forecast.1.precipitation',
+            attribute: 'forecast.2.precipitation',
             iobId: deviceId + '.TotalLiquidVolume_d3',
             initialValue: 501,
             updatedValue: 601
         }, {
-            attribute: 'forecast.1.datetime',
+            attribute: 'forecast.2.datetime',
             iobId: deviceId + '.DateTime_d3',
             initialValue: 'Fr',
             updatedValue: 'Sa'
         }, {
-            attribute: 'forecast.2.condition',
+            attribute: 'forecast.3.condition',
             iobId: deviceId + '.WeatherIconURL_d4',
             initialValue: 'iconURL3',
             updatedValue: 'iconURL3Updated'
         }, {
-            attribute: 'forecast.2.temperature',
+            attribute: 'forecast.3.temperature',
             iobId: deviceId + '.TempMax_d4',
             initialValue: 14,
             updatedValue: 15
         }, {
-            attribute: 'forecast.2.templow',
+            attribute: 'forecast.3.templow',
             iobId: deviceId + '.TempMin_d4',
             initialValue: 4,
             updatedValue: 5
         }, {
-            attribute: 'forecast.2.precipitation_probability',
+            attribute: 'forecast.3.precipitation_probability',
             iobId: deviceId + '.PrecipitationProbability_d4',
             initialValue: 52,
             updatedValue: 62
         }, {
-            attribute: 'forecast.2.precipitation',
+            attribute: 'forecast.3.precipitation',
             iobId: deviceId + '.TotalLiquidVolume_d4',
             initialValue: 502,
             updatedValue: 602
         }, {
-            attribute: 'forecast.2.datetime',
+            attribute: 'forecast.3.datetime',
             iobId: deviceId + '.DateTime_d4',
             initialValue: 'Sa',
             updatedValue: 'So'
         }, {
-            attribute: 'forecast.3.condition',
+            attribute: 'forecast.4.condition',
             iobId: deviceId + '.WeatherIconURL_d5',
             initialValue: 'iconURL4',
             updatedValue: 'iconURL4Updated'
         }, {
-            attribute: 'forecast.3.temperature',
+            attribute: 'forecast.4.temperature',
             iobId: deviceId + '.TempMax_d5',
             initialValue: 20,
             updatedValue: 21
         }, {
-            attribute: 'forecast.3.templow',
+            attribute: 'forecast.4.templow',
             iobId: deviceId + '.TempMin_d5',
             initialValue: 10,
             updatedValue: 12
         }, {
-            attribute: 'forecast.3.precipitation_probability',
+            attribute: 'forecast.4.precipitation_probability',
             iobId: deviceId + '.PrecipitationProbability_d5',
             initialValue: 53,
             updatedValue: 63
         }, {
-            attribute: 'forecast.3.precipitation',
+            attribute: 'forecast.4.precipitation',
             iobId: deviceId + '.TotalLiquidVolume_d5',
             initialValue: 503,
             updatedValue: 603
         }, {
-            attribute: 'forecast.3.datetime',
+            attribute: 'forecast.4.datetime',
             iobId: deviceId + '.DateTime_d5',
             initialValue: 'So',
             updatedValue: 'Mo'
@@ -203,91 +233,106 @@ exports.runTests = function (getHarness) {
             updatedValue: 30
         }, {
             attribute: 'forecast.0.condition',
+            iobId: deviceId + '.Day_1.iconURL',
+            initialValue: 'iconURL',
+            updatedValue: 'iconURLUpdated'
+        }, {
+            attribute: 'forecast.0.temperature',
+            iobId: deviceId + '.Day_1.Maximale_Temperatur_value',
+            initialValue: 25,
+            updatedValue: 30
+        }, {
+            attribute: 'forecast.0.templow',
+            iobId: deviceId + '.Day_1.Minimale_Temperatur_value',
+            initialValue: 7,
+            updatedValue: 6
+        }, {
+            attribute: 'forecast.1.condition',
             iobId: deviceId + '.Day_2.iconURL',
             initialValue: 'iconURL1',
             updatedValue: 'iconURL1Updated'
         }, {
-            attribute: 'forecast.0.temperature',
+            attribute: 'forecast.1.temperature',
             iobId: deviceId + '.Day_2.Maximale_Temperatur_value',
             initialValue: 10,
             updatedValue: 11
         }, {
-            attribute: 'forecast.0.templow',
+            attribute: 'forecast.1.templow',
             iobId: deviceId + '.Day_2.Minimale_Temperatur_value',
             initialValue: 8,
             updatedValue: 9
         }, {
-            attribute: 'forecast.1.condition',
+            attribute: 'forecast.2.condition',
             iobId: deviceId + '.Day_3.iconURL',
             initialValue: 'iconURL2',
             updatedValue: 'iconURL2Updated'
         }, {
-            attribute: 'forecast.1.temperature',
+            attribute: 'forecast.2.temperature',
             iobId: deviceId + '.Day_3.Maximale_Temperatur_value',
             initialValue: 12,
             updatedValue: 13
         }, {
-            attribute: 'forecast.1.templow',
+            attribute: 'forecast.2.templow',
             iobId: deviceId + '.Day_3.Minimale_Temperatur_value',
             initialValue: 7,
             updatedValue: 8
         }, {
-            attribute: 'forecast.2.condition',
+            attribute: 'forecast.3.condition',
             iobId: deviceId + '.Day_4.iconURL',
             initialValue: 'iconURL3',
             updatedValue: 'iconURL3Updated'
         }, {
-            attribute: 'forecast.2.temperature',
+            attribute: 'forecast.3.temperature',
             iobId: deviceId + '.Day_4.Maximale_Temperatur_value',
             initialValue: 14,
             updatedValue: 15
         }, {
-            attribute: 'forecast.2.templow',
+            attribute: 'forecast.3.templow',
             iobId: deviceId + '.Day_4.Minimale_Temperatur_value',
             initialValue: 4,
             updatedValue: 5
         }, {
-            attribute: 'forecast.3.condition',
+            attribute: 'forecast.4.condition',
             iobId: deviceId + '.Day_5.iconURL',
             initialValue: 'iconURL4',
             updatedValue: 'iconURL4Updated'
         }, {
-            attribute: 'forecast.3.temperature',
+            attribute: 'forecast.4.temperature',
             iobId: deviceId + '.Day_5.Maximale_Temperatur_value',
             initialValue: 20,
             updatedValue: 21
         }, {
-            attribute: 'forecast.3.templow',
+            attribute: 'forecast.4.templow',
             iobId: deviceId + '.Day_5.Minimale_Temperatur_value',
             initialValue: 10,
             updatedValue: 12
         }, {
-            attribute: 'forecast.4.condition',
+            attribute: 'forecast.5.condition',
             iobId: deviceId + '.Day_6.iconURL',
             initialValue: 'iconURL5',
             updatedValue: 'iconURL5Updated'
         }, {
-            attribute: 'forecast.4.temperature',
+            attribute: 'forecast.5.temperature',
             iobId: deviceId + '.Day_6.Maximale_Temperatur_value',
             initialValue: 30,
             updatedValue: 31
         }, {
-            attribute: 'forecast.4.templow',
+            attribute: 'forecast.5.templow',
             iobId: deviceId + '.Day_6.Minimale_Temperatur_value',
             initialValue: 13,
             updatedValue: 14
         }, {
-            attribute: 'forecast.5.condition',
+            attribute: 'forecast.6.condition',
             iobId: deviceId + '.Day_7.iconURL',
             initialValue: 'iconURL6',
             updatedValue: 'iconURL6Updated'
         }, {
-            attribute: 'forecast.5.temperature',
+            attribute: 'forecast.6.temperature',
             iobId: deviceId + '.Day_7.Maximale_Temperatur_value',
             initialValue: 25,
             updatedValue: 28
         }, {
-            attribute: 'forecast.5.templow',
+            attribute: 'forecast.6.templow',
             iobId: deviceId + '.Day_7.Minimale_Temperatur_value',
             initialValue: 15,
             updatedValue: 17
@@ -304,13 +349,13 @@ exports.runTests = function (getHarness) {
             }
         }
 
-        for (let day = 0; day < 6; day += 1) {
+        for (let day = 0; day < entity.attributes.forecast.length; day += 1) {
             const forecast = entity.attributes.forecast[day];
             expect(forecast).to.have.property('datetime');
             const date = new Date(forecast.datetime);
             expect(date).to.be.ok;
             const now = new Date();
-            expect(date.getDate()).to.equal(now.getDate() + day + 1);
+            expect(date.getDate()).to.equal(now.getDate() + day);
         }
 
         await testUpdates(harness, states, entity);
@@ -319,7 +364,7 @@ exports.runTests = function (getHarness) {
         await tools.validateStateChange(harness, entity.entity_id,
             async () => await harness.states.setStateAsync(deviceId + '.Day_7.iconURL', {val: 'icon6URLNeu', ts: newTS, ack: true}),
             entity => {
-                const date = new Date(entity.attributes.forecast[5].datetime);
+                const date = new Date(entity.attributes.forecast[6].datetime);
                 const controlDate = new Date(newTS);
                 expect(date.getDate()).to.equal(controlDate.getDate() +6);
             });
@@ -362,91 +407,121 @@ exports.runTests = function (getHarness) {
             updatedValue: 180
         }, {
             attribute: 'forecast.0.condition',
+            iobId: deviceId + '.0d.iconURL',
+            initialValue: 'iconURL',
+            updatedValue: 'iconURLUpdated'
+        }, {
+            attribute: 'forecast.0.temperature',
+            iobId: deviceId + '.0d.tempMax',
+            initialValue: 9,
+            updatedValue: 10
+        }, {
+            attribute: 'forecast.0.templow',
+            iobId: deviceId + '.0d.tempMin',
+            initialValue: 7,
+            updatedValue: 8
+        }, {
+            attribute: 'forecast.0.precipitation_probability',
+            iobId: deviceId + '.0d.precipitationChance',
+            initialValue: 40,
+            updatedValue: 50
+        }, {
+            attribute: 'forecast.0.datetime',
+            iobId: deviceId + '.0d.date',
+            initialValue: 'Mi',
+            updatedValue: 'Do'
+        }, {
+            attribute: 'forecast.0.humidity',
+            iobId: deviceId + '.0d.humidity',
+            initialValue: 30,
+            updatedValue: 40
+        }, {
+            attribute: 'forecast.1.condition',
             iobId: deviceId + '.1d.iconURL',
             initialValue: 'iconURL1',
             updatedValue: 'iconURL1Updated'
         }, {
-            attribute: 'forecast.0.temperature',
+            attribute: 'forecast.1.temperature',
             iobId: deviceId + '.1d.tempMax',
             initialValue: 10,
             updatedValue: 11
         }, {
-            attribute: 'forecast.0.templow',
+            attribute: 'forecast.1.templow',
             iobId: deviceId + '.1d.tempMin',
             initialValue: 8,
             updatedValue: 9
         }, {
-            attribute: 'forecast.0.precipitation_probability',
+            attribute: 'forecast.1.precipitation_probability',
             iobId: deviceId + '.1d.precipitationChance',
             initialValue: 50,
             updatedValue: 60
         }, {
-            attribute: 'forecast.0.datetime',
+            attribute: 'forecast.1.datetime',
             iobId: deviceId + '.1d.date',
             initialValue: 'Do',
             updatedValue: 'Fr'
         }, {
-            attribute: 'forecast.0.humidity',
+            attribute: 'forecast.1.humidity',
             iobId: deviceId + '.1d.humidity',
             initialValue: 34,
             updatedValue: 45
         }, {
-            attribute: 'forecast.1.condition',
+            attribute: 'forecast.2.condition',
             iobId: deviceId + '.2d.iconURL',
             initialValue: 'iconURL2',
             updatedValue: 'iconURL2Updated'
         }, {
-            attribute: 'forecast.1.temperature',
+            attribute: 'forecast.2.temperature',
             iobId: deviceId + '.2d.tempMax',
             initialValue: 12,
             updatedValue: 13
         }, {
-            attribute: 'forecast.1.templow',
+            attribute: 'forecast.2.templow',
             iobId: deviceId + '.2d.tempMin',
             initialValue: 7,
             updatedValue: 8
         }, {
-            attribute: 'forecast.1.precipitation_probability',
+            attribute: 'forecast.2.precipitation_probability',
             iobId: deviceId + '.2d.precipitationChance',
             initialValue: 51,
             updatedValue: 61
         }, {
-            attribute: 'forecast.1.datetime',
+            attribute: 'forecast.2.datetime',
             iobId: deviceId + '.2d.date',
             initialValue: 'Fr',
             updatedValue: 'Sa'
         }, {
-            attribute: 'forecast.1.humidity',
+            attribute: 'forecast.2.humidity',
             iobId: deviceId + '.2d.humidity',
             initialValue: 51,
             updatedValue: 60
         }, {
-            attribute: 'forecast.2.condition',
+            attribute: 'forecast.3.condition',
             iobId: deviceId + '.3d.iconURL',
             initialValue: 'iconURL3',
             updatedValue: 'iconURL3Updated'
         }, {
-            attribute: 'forecast.2.temperature',
+            attribute: 'forecast.3.temperature',
             iobId: deviceId + '.3d.tempMax',
             initialValue: 14,
             updatedValue: 15
         }, {
-            attribute: 'forecast.2.templow',
+            attribute: 'forecast.3.templow',
             iobId: deviceId + '.3d.tempMin',
             initialValue: 4,
             updatedValue: 5
         }, {
-            attribute: 'forecast.2.precipitation_probability',
+            attribute: 'forecast.3.precipitation_probability',
             iobId: deviceId + '.3d.precipitationChance',
             initialValue: 52,
             updatedValue: 62
         }, {
-            attribute: 'forecast.2.datetime',
+            attribute: 'forecast.3.datetime',
             iobId: deviceId + '.3d.date',
             initialValue: 'Sa',
             updatedValue: 'So'
         }, {
-            attribute: 'forecast.2.humidity',
+            attribute: 'forecast.3.humidity',
             iobId: deviceId + '.3d.humidity',
             initialValue: 32,
             updatedValue: 41
