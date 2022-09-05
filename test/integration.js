0d1ce5749929753b33395d6e855654668a974611
Garfonso <garfonso@mobo.info>
2022-09-05 10:50:11 +0200

    use options of new testing
    (sometimes it is necessary to change controller version here *cough*)

--- test/integration.js
+++ test/integration.js
@@ -11,9 +11,16 @@ tests.integration(path.join(__dirname, '..'), {
     // By default, termination during startup is not allowed.
     //allowedExitCodes: [11],
 
+    // To test against a different version of JS-Controller, you can change the version or dist-tag here.
+    // Make sure to remove this setting when you're done testing.
+    controllerVersion: 'dev', // 'latest', 'dev' or a specific version like '4.0.23'
+
+    /** The loglevel to use for DB and adapter related logs */
+    loglevel: 'debug',
+
     // Define your own tests inside defineAdditionalTests
-    // Since the tests are heavily instrumented, you need to create and use a so called "harness" to control the tests.
-    defineAdditionalTests(getHarness) {
-        integrationTests.runTests(getHarness);
+    // Since the tests are heavily instrumented, you need to create and use a so-called "harness" to control the tests.
+    defineAdditionalTests({ suite }) {
+        integrationTests.runTests(suite);
     }
 });
