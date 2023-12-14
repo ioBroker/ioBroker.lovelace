2cfb88206dfde36c0481fc75b42fab2a837a4b75
Garfonso <garfonso@mobo.info>
2023-12-14 11:46:20 +0100

    Update newFrontend.sh

--- hass_frontend/static_cards/newFrontend.sh
+++ hass_frontend/static_cards/newFrontend.sh
@@ -6,4 +6,4 @@ rm -rf frontend_* static *.py *.html py.typed robots.txt *.js
 
 cp -r ../../home-assistant-polymer/hass_frontend/* .
 # will try to read version from this:
-cp -r ../../home-assistant-polymer/pyproject.toml .
+cat ../../home-assistant-polymer/pyproject.toml | grep version | cut -d'"' -f2 > version.txt
