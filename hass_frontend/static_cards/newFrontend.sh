b433ef3f4c8be39e77f01046a0ee3643c84b48ba
Garfonso <garfonso@mobo.info>
2023-12-14 11:42:17 +0100

    Update newFrontend.sh

--- hass_frontend/static_cards/newFrontend.sh
+++ hass_frontend/static_cards/newFrontend.sh
@@ -1,7 +1,9 @@
 #/bin/bash
 
-cd ~/development/iobroker/ioBroker.lovelace/hass_frontend/
+cd hass_frontend/ || { echo "Error, please run script from root of the git project."; exit 1; }
 
 rm -rf frontend_* static *.py *.html py.typed robots.txt *.js
 
-cp -r ~/development/iobroker/home-assistant-polymer/hass_frontend/* .
+cp -r ../../home-assistant-polymer/hass_frontend/* .
+# will try to read version from this:
+cp -r ../../home-assistant-polymer/pyproject.toml .
