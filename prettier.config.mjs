2e9ef286764b6e2eac2f1179d2acaf4e398f166f
Garfonso <garfonso@mobo.info>
2024-11-25 10:45:15 +0100

    more responsive now?
    trying to work on #558

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+// iobroker prettier configuration file
+import prettierConfig from '@iobroker/eslint-config/prettier.config.mjs';
+
+export default {
+    ...prettierConfig,
+    // uncomment the next line if you prefer double quotes
+    // singleQuote: false,
+    trailingComma: 'none'
+}
